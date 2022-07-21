e2ee6570bc25bc79509d5baa2f18cf05464360e9
dassepascal <pascaldasse56@gmail.com>
2022-07-21 14:16:16 +0200

    init

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+
+import app from './components/app.vue';
+console.log(app);
+
+createApp(app).mount('#app')
---
+++ resources/js/components/app.vue
@@ -0,0 +1,3 @@
+<template>
+<h1> Laravel 9, vite and vue js </h1>
+</template>
